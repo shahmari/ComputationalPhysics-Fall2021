0fa83e24131c24e65146678dbb59f338ed25ba98
shahmari <shahmari.acer@outlook.com>
2021-10-11 12:24:49 +0330

    coloring alghotitm comleted.

--- ProblemSet3/Codes/Q3/coloring-alghorithm.jl
+++ ProblemSet3/Codes/Q3/coloring-alghorithm.jl
@@ -1,16 +1,5 @@
 using Plots
 
-P = 0.01
-dim = 100
-maxnum = 1000
-
-Network = rand((0,-1),dim,dim)
-
-Network[1,:] = fill(1,dim)
-Network[end,:] = fill(maxnum,dim)
-
-Num = 2
-
 function ColoringTheNode(i, j, Network, Num)
     if P > rand() && Network[i, j] == 0
         Network[i, j] = Num
@@ -19,25 +8,11 @@ function ColoringTheNode(i, j, Network, Num)
     return Network, Num
 end
 
-for i in 2:dim-1
-    for j in 1:dim
-        Network, Num = ColoringTheNode(i, j, Network, Num)
-        colorList, Numbereds = FindNeighbors(i, j, Network)
-        if Numbereds == 0
-            continue
-        elseif Numbereds == 1
-            Network[i, j] = colorList[2]
-        else
-            Network = JoinColors(Network, dim, colorList)
-        end
-    end
-end
-
-function FindNeighbors(i, j, Network)
+function FindNeighbors(i, j, Network, dim)
     Numbereds = 0
     colorList = [Network[i, j]]
     for neighbor in ([0,1], [1,0], [0,-1], [-1,0])
-        if Network[([i, j] + neighbor)...] > 0
+        if j + neighbor[2] in 1:dim && Network[([i, j] + neighbor)...] > 0
             push!(colorList,Network[([i, j] + neighbor)...])
             Numbereds += 1
         end
@@ -57,4 +32,27 @@ function JoinColors(Network, dim, colorList)
     return Network
 end
 
-Network = rand(1:3,10,10)
+P = 0.01
+dim = 100
+maxnum = 1000
+
+Network = rand((0,-1),dim,dim)
+
+Network[1,:] = fill(1,dim)
+Network[end,:] = fill(maxnum,dim)
+
+Num = 2
+
+for i in 2:dim-1
+    for j in 1:dim
+        Network, Num = ColoringTheNode(i, j, Network, Num)
+        colorList, Numbereds = FindNeighbors(i, j, Network, dim)
+        if Numbereds == 0
+            continue
+        elseif Numbereds == 1
+            Network[i, j] = colorList[2]
+        else
+            Network = JoinColors(Network, dim, colorList)
+        end
+    end
+end
