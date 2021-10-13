78a7a75e9cd0a2e6915ff9da59ba43a190016d83
shahmari <shahmari.acer@outlook.com>
2021-10-13 16:12:03 +0330

    make HK better

--- ProblemSet3/Codes/Q5/Q5-HK.jl
+++ ProblemSet3/Codes/Q5/Q5-HK.jl
@@ -24,48 +24,56 @@ function RecursionMapping(x, L)
     return L[x]
 end
 
-dim = 7
-
-Network = InitialNetwork(dim, 0.5)
-
-Network =    [0   0   0  -1   0  -1   0
-              0   0   0  -1  -1   0   0
-             -1  -1   0   0  -1   0  -1
-              0  -1  -1   0   0  -1  -1
-             -1  -1   0   0   0   0   0
-             -1  -1  -1   0  -1   0   0
-              0   0   0   0  -1   0   0]
-
-S = []
-L = []
-
-k = 1
-
-for j in 1:dim
+function CheckPercolation(dim, L, Network)
+    fside = []
+    lside = []
     for i in 1:dim
-        if Network[i,j] != 0
-            continue
+        if Network[i,dim] != -1
+            push!(lside, RecursionMapping(Network[i,dim],L))
         end
-        NColor, NNum = FindNeighbors(i,j,Network,dim)
-        if NNum == 0
-            Network[i,j] = k
-            push!(L, k)
-            push!(S, 1)
-            k += 1
-        elseif NNum == 1
-            Network[i,j] = RecursionMapping(NColor[1],L)
-            S[RecursionMapping(NColor[1],L)] += 1
-        elseif NNum == 2 && RecursionMapping(NColor[1],L) == RecursionMapping(NColor[2],L)
-            Network[i,j] = RecursionMapping(NColor[1],L)
-            S[RecursionMapping(NColor[1],L)] += 1
-        else
-            S[RecursionMapping(NColor[1],L)] += 1 + S[RecursionMapping(NColor[2],L)]
-            S[RecursionMapping(NColor[2],L)] = 0
-            Network[i,j] = RecursionMapping(NColor[1],L)
-            L[NColor[2]] = RecursionMapping(NColor[1],L)
+        if Network[i,1] != -1
+            push!(fside, RecursionMapping(Network[i,1],L))
         end
+    end
+    if length(intersect(fside,lside)) > 0
+        return 1
+    else
+        return 0
     end
 end
 
+function HKNetworkDynamic(dim, P)
+    Network = InitialNetwork(dim, P)
+    S = []
+    L = []
+    k = 1
 
-RecursionMapping(5, [1,2,3,3,4])
+    for j in 1:dim
+        for i in 1:dim
+            if Network[i,j] != 0
+                continue
+            end
+            NColor, NNum = FindNeighbors(i,j,Network,dim)
+            if NNum == 0
+                Network[i,j] = k
+                push!(L, k)
+                push!(S, 1)
+                k += 1
+            elseif NNum == 1
+                Network[i,j] = RecursionMapping(NColor[1],L)
+                S[RecursionMapping(NColor[1],L)] += 1
+            elseif NNum == 2 && RecursionMapping(NColor[1],L) == RecursionMapping(NColor[2],L)
+                Network[i,j] = RecursionMapping(NColor[1],L)
+                S[RecursionMapping(NColor[1],L)] += 1
+            else
+                S[RecursionMapping(NColor[1],L)] += 1 + S[RecursionMapping(NColor[2],L)]
+                S[RecursionMapping(NColor[2],L)] = 0
+                Network[i,j] = RecursionMapping(NColor[1],L)
+                L[NColor[2]] = RecursionMapping(NColor[1],L)
+            end
+        end
+    end
+    return Network
+end
+
+HKNetworkDynamic(200, 0.65)
