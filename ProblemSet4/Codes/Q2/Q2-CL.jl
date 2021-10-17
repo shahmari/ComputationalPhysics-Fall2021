96fceae9494da2c6180ddef85ff64dd8a3720afb
shahmari <shahmari.acer@outlook.com>
2021-10-17 18:13:09 +0330

    add Q2 code

---
+++ ProblemSet4/Codes/Q2/Q2-CL.jl
@@ -0,0 +1,72 @@
+using Plots, StatsBase, Statistics, LaTeXStrings, JLD
+
+function InitialNetwork(dim, P)
+    Network = sample([-1,0], Weights([1-P, P]),(dim,dim))
+    return Network
+end
+
+function FindNeighbors(i, j, Network, dim)
+    Numbereds = 0
+    colorList = []
+    for neighbor in ([0,-1], [-1,0])
+        if j + neighbor[2] >= 1 && i + neighbor[1] >= 1 && Network[([i, j] + neighbor)...] > 0
+            push!(colorList,Network[([i, j] + neighbor)...])
+            Numbereds += 1
+        end
+    end
+    return colorList, Numbereds #colorList contain the node and its neighbors
+end
+
+function RecursionMapping(x, L)
+    while L[x] != L[L[x]]
+        x = L[x]
+    end
+    return L[x]
+end
+
+@inline function HKNetworkDynamic(dim, P)
+    Network = InitialNetwork(dim, P)
+    S = []
+    L = []
+    k = 1
+
+    for j in 1:dim
+        for i in 1:dim
+            if Network[i,j] != 0
+                continue
+            end
+            NColor, NNum = FindNeighbors(i,j,Network,dim)
+            if NNum == 0
+                Network[i,j] = k
+                push!(L, k)
+                push!(S, 1)
+                k += 1
+            elseif NNum == 1
+                Network[i,j] = RecursionMapping(NColor[1],L)
+                S[RecursionMapping(NColor[1],L)] += 1
+            elseif NNum == 2 && RecursionMapping(NColor[1],L) == RecursionMapping(NColor[2],L)
+                Network[i,j] = RecursionMapping(NColor[1],L)
+                S[RecursionMapping(NColor[1],L)] += 1
+            else
+                S[RecursionMapping(NColor[1],L)] += 1 + S[RecursionMapping(NColor[2],L)]
+                S[RecursionMapping(NColor[2],L)] = 0
+                Network[i,j] = RecursionMapping(NColor[1],L)
+                L[NColor[2]] = RecursionMapping(NColor[1],L)
+            end
+        end
+    end
+    return Network, S, L
+    # return CheckPercolation(dim, L, Network)
+end
+
+function JoinColors(Network, L)
+    dim = size(Network)[1]
+    for i in 1:dim
+        for j in 1:dim
+            if Network[i,j]!= -1
+                Network[i,j] = RecursionMapping(Network[i,j],L)
+            end
+        end
+    end
+    return Network
+end
