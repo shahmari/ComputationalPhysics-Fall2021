09488beca2a4e359a35c9b2b6bd343bace0b3f80
shahmari <shahmari.acer@outlook.com>
2021-10-22 20:31:57 +0330

    start Q3. create initial net and find border

---
+++ ProblemSet4/Codes/Q3/Q3-CG.jl
@@ -0,0 +1,25 @@
+using Plots, StatsBase, Statistics, LaTeXStrings, JLD
+
+dim = 100
+
+Network = zeros(Int,dim,dim)
+Network[rand(1:dim^2)] = 1
+
+border0 = []
+for i in 1:dim
+    for j in 1:dim
+        check = false
+        if Network[i,j] == 0
+            Neighbors = ((i-1,j),(i,j-1),(i+1,j),(i,j+1))
+            for nei in Neighbors
+                if Network[nei...] == 1
+                    check = true
+                    break
+                end
+            end
+            if check == true
+                push!(border0,(i,j))
+            end
+        end
+    end
+end
