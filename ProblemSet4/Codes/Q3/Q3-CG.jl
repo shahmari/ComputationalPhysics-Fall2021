6f5b9a505615a602e3600a1e1be21eaae6706d3d
shahmari <shahmari.acer@outlook.com>
2021-10-22 21:47:50 +0330

    finished i hope

--- ProblemSet4/Codes/Q3/Q3-CG.jl
+++ ProblemSet4/Codes/Q3/Q3-CG.jl
@@ -1,10 +1,5 @@
 using Plots, StatsBase, Statistics, LaTeXStrings, JLD
 
-dim = 100
-
-Network = zeros(Int,dim,dim)
-Network[rand(1:dim^2)] = 1
-
 function FindBorder(dim, Network)
     border0 = []
     for i in 1:dim
@@ -27,4 +22,36 @@ function FindBorder(dim, Network)
     return border0
 end
 
-FindBorder(dim,Network)
+function FindCorrelationLength(Network,dim)
+    FindOnSpots = findall(x->x==1,Network)
+    SpotNums = length(FindOnSpots)
+    ΣR² = 0.0; iMC = 0.0; jMC = 0.0
+    for indx in FindOnSpots
+        iMC += indx[1]/SpotNums
+        jMC += indx[2]/SpotNums
+    end
+    for indx in FindOnSpots
+        ΣR² += ((indx[1]-iMC)^2 + (indx[2]-jMC)^2)/SpotNums
+    end
+    return sqrt(ΣR²)
+end
+
+function ClusterGrowth(dim,P)
+    Network = zeros(Int,dim,dim)
+    # Network[rand(1:dim^2)] = 1
+    Network[round(Int, dim/2), round(Int, dim/2)] = 1
+    while 0 in Network
+        Borders = FindBorder(dim,Network)
+        if length(Borders) == 0
+            break
+        end
+        for spot in Borders
+            if P > rand()
+                Network[spot...] = 1
+            else
+                Network[spot...] = -1
+            end
+        end
+    end
+    return Network
+end
