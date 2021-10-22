18ad075163cc6eaf5d65e1078888f23d9fe0a596
shahmari <shahmari.acer@outlook.com>
2021-10-23 00:05:00 +0330

    export plots

--- ProblemSet4/Codes/Q3/Q3-CG.jl
+++ ProblemSet4/Codes/Q3/Q3-CG.jl
@@ -1,4 +1,4 @@
-using Plots, StatsBase, Statistics, LaTeXStrings, JLD
+using Plots, StatsBase, Statistics, LaTeXStrings, JLD, StatsPlots
 
 function FindBorder(dim, Network)
     border0 = []
@@ -54,17 +54,58 @@ function ClusterGrowth(dim,P)
     return Network
 end
 
-Network = ClusterGrowth(200,0.56)
-heatmap(Network, legend = nothing)
+# Network = ClusterGrowth(200,0.56)
+# heatmap(Network, legend = nothing)
 
-runnum = 1000
-for p in [0.5,0.55,0.59]
-    totstepdata = []
-    for i in 1:runnum
-        Network = ClusterGrowth(200,p)
-        push!(totstepdata,(FindCorrelationLength(Network,200),length(findall(x-> x==1,Network))))
-        print("\r$i")
-    end
-    println('#')
-    save("../../Data/Q3/Q3-$p-CG.jld", "data", totstepdata)
-end
+# runnum = 10000
+# for p in [0.5,0.55,0.59]
+#     totstepdata = []
+#     for i in 1:runnum
+#         Network = ClusterGrowth(200,p)
+#         push!(totstepdata,(FindCorrelationLength(Network,200),length(findall(x-> x==1,Network))))
+#         print("\r$i")
+#     end
+#     println('#')
+#     save("../../Data/Q3/Q3-$p-CG.jld", "data", totstepdata)
+# end
+
+# AllXiData = []
+# AllSData = []
+# for p in [0.5,0.55,0.59]
+#     Data = load("../../Data/Q3/Q3-$p-CG.jld")["data"]
+#     xidata = []
+#     sdata = []
+#     for data in Data
+#         push!(xidata, data[1])
+#         push!(sdata, data[2])
+#     end
+#     push!(AllXiData,xidata)
+#     push!(AllSData,sdata)
+# end
+# AllXiData
+violin(["0.59"],AllXiData[3])
+violin!(["0.55"],AllXiData[2])
+violin!(["0.50"],AllXiData[1])
+plot!(legend = nothing, title = L"Correlation\ Length\ for\ 200\times200\ Network,\ 10000 runs",xlabel = L"P",ylabel=L"\xi_{(P)}")
+savefig("../../Figs/Q3/Q3-xi-violon.pdf")
+
+
+histogram(AllXiData[3],bins = 50, label = L"P=0.59")
+histogram!(AllXiData[2],bins = 50, label = L"P=0.55")
+histogram!(AllXiData[1],bins = 25, label = L"P=0.50")
+plot!(title = L"Correlation\ Length\ for\ 200\times200\ Network,\ 10000 runs",xlabel = L"\xi_{(P)}",ylabel=L"Numbers")
+savefig("../../Figs/Q3/Q3-xi-hist.pdf")
+
+
+violin(["0.59"],AllSData[3])
+violin!(["0.55"],AllSData[2])
+violin!(["0.50"],AllSData[1])
+plot!(legend = nothing, title = L"Cluster\ Size\ for\ 200\times200\ Network,\ 10000 runs",xlabel = L"P",ylabel=L"\S_{(P)}")
+savefig("../../Figs/Q3/Q3-S-violon.pdf")
+
+
+histogram(AllXiData[3],bins = 50, label = L"P=0.59")
+histogram!(AllXiData[2],bins = 50, label = L"P=0.55")
+histogram!(AllXiData[1],bins = 25, label = L"P=0.50")
+plot!(title = L"Cluster\ Size\ for\ 200\times200\ Network,\ 10000 runs",xlabel = L"\S_{(P)}",ylabel=L"Numbers")
+savefig("../../Figs/Q3/Q3-S-hist.pdf")
