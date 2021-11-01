f693f2f7b1146c9d833d8e0557ab90bf49a63450
shahmari <shahmari.acer@outlook.com>
2021-11-01 20:53:15 +0330

    sorry I forgot this one.

--- ProblemSet5/Codes/Q1/Q1-2DRW.jl
+++ ProblemSet5/Codes/Q1/Q1-2DRW.jl
@@ -26,17 +26,21 @@ function PlotHeatmap(Data, RWNumbers) #useless piece of shit
     return heatmap(Surf)
 end
 =#
-TotalSteps = 1000
+
+TStepsList = [10,100,1000]
 RWNumbers = 100000
 R₀ = [0,0]
-
-Data = zeros(Int,RWNumbers,2)
-for i ∈ ProgressBar(1:RWNumbers)
-    Data[i,:] = RandomWalker2D(TotalSteps,R₀)
+Data = []
+for TotalSteps in TStepsList
+    TotalWalkers = zeros(Int,RWNumbers,2)
+    for i ∈ ProgressBar(1:RWNumbers)
+        TotalWalkers[i,:] = RandomWalker2D(TotalSteps,R₀)
+    end
+    push!(Data, copy(TotalWalkers))
 end
+save("../../Data/Q1/Q1-2DHist.jld", "Data", Data)
 
-
-
+Data
 
 
 RgList = [RGyration(Data[:,i,:],RWNumbers) for i ∈ 1:TotalSteps]
@@ -49,23 +53,26 @@ RgList = [RGyration(Data[:,i,:],RWNumbers) for i ∈ 1:TotalSteps]
 scatter(RgList.^2)
 
 # PlotHeatmap(Data, RWNumbers)
-
-P1 = histogram2d(collect(Tuple(Data[i,:]) for i ∈ 1:RWNumbers),
+dimnum = 3
+P1 = histogram2d(collect(Tuple(Data[dimnum][i,:]) for i ∈ 1:RWNumbers),
     nbin = 200,
     xaxis = nothing,
     yaxis = nothing,
     background=:black,
     framestyle = :box
     )
-P2 = histogram(Data[:,2], orientation = :horizontal, xticks = [0,3500],
-    flip = true,framestyle = :box, c = :purple)
-P3 = histogram(Data[:,1], flip = true, yticks = [0,3500],
-    framestyle = :box, c = :purple)
+P2 = histogram(Data[dimnum][:,2], orientation = :horizontal, xticks = [0,3000],
+    ylabel = L"Y", flip = true,framestyle = :box, c = :purple)
+P3 = histogram(Data[dimnum][:,1], flip = true, yticks = [0,3000],
+    xlabel = L"X", framestyle = :box, c = :purple)
 P4 = plot(legend=false,grid=false,
     foreground_color_subplot=:black,background=:black,framestyle = :box)
 
-P21 = plot(P2,P1,layout = @layout[a{0.15w} grid(1,1, heights = [0.95])], legend = nothing)
+P21 = plot(P2,P1,layout = @layout[a{0.1w} grid(1,1, heights = [0.95])], legend = nothing)
 
 P22 = plot(P4,P3,P4,layout = @layout[a{0.08w} grid(1,1) a{0.02w}], legend = nothing)
 
-plot(P21,P22,layout = grid(2,1, heights = [0.85,0.15]))
+plot(P21,P22,layout = grid(2,1, heights = [0.85,0.15]),
+    plot_title = L"Distribution\ of\ %$RWNumbers\ RW\ in\ %$(TStepsList[dimnum])\ steps")
+
+savefig("../../Figs/Q1/Q1-2DHist$dimnum.pdf")
