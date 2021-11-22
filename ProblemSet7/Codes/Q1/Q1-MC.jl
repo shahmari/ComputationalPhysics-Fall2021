8d419b7559f15b9ff3594594a7994a0f4540038f
shahmari <shahmari.acer@outlook.com>
2021-11-22 20:15:07 +0330

    Update Q1-MC.jl

--- ProblemSet7/Codes/Q1/Q1-MC.jl
+++ ProblemSet7/Codes/Q1/Q1-MC.jl
@@ -1,17 +1,17 @@
-using Plots, StatsBase, LaTeXStrings, JLD
+using Plots, Statistics, LaTeXStrings, JLD
 
 
-function SMC(;f::Function, X₀::Integer, X₁::Integer, Ŋ::Integer = 100000)
+function SMC(; f::Function, X₀::Real, X₁::Real, Ŋ::Integer = 100000)
     ΔX = X₁ - X₀
-    fₓ = f.(ΔX.*rand(Ŋ) .+ X₀)
+    fₓ = f.(ΔX .* rand(Ŋ) .+ X₀)
     Avg = mean(fₓ)
     ∫fₓdx = Avg * ΔX
     Δ = std(fₓ) / √Ŋ
     return ∫fₓdx, Δ
 end
 
-function IMC(;f::Function, g::Function, ∫gₓdx::Real, Đₓ::Function, Ŋ::Integer = 100000)
-    fₓ╱gₓ = x -> f(x)/g(x)
+function IMC(; f::Function, g::Function, ∫gₓdx::Real, Đₓ::Function, Ŋ::Integer = 100000)
+    fₓ╱gₓ = x -> f(x) / g(x)
     Samples = fₓ╱gₓ.(Đₓ.(rand(Ŋ)))
     Avg = mean(Samples)
     σ = std(Samples)
@@ -20,8 +20,8 @@ function IMC(;f::Function, g::Function, ∫gₓdx::Real, Đₓ::Function, Ŋ::In
     return ∫fₓdx, Δ
 end
 
-SMCParameters = Dict(:f => x -> ℯ^(- x^2), :X₀ => 0, :X₁ => 2, :Ŋ => 10^7)
-IMCParameters = Dict(:f => x -> ℯ^(- x^2), :g => x -> ℯ^-x, :∫gₓdx => 1 - ℯ^-2, :Đₓ => x -> -log(1 - (1 - ℯ^-2) * x), :Ŋ => 10^7)
+SMCParameters = Dict(:f => x -> ℯ^(-x^2), :X₀ => 0, :X₁ => 2, :Ŋ => 10^7)
+IMCParameters = Dict(:f => x -> ℯ^(-x^2), :g => x -> ℯ^-x, :∫gₓdx => 1 - ℯ^-2, :Đₓ => x -> -log(1 - (1 - ℯ^-2) * x), :Ŋ => 10^7)
 
-SMC(;SMCParameters...)
-IMC(;IMCParameters...)
+SMC(; SMCParameters...)
+IMC(; IMCParameters...)
