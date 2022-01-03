55f8cdfd3320f68b1c9e0705215c43e1f0a75620
shahmari <shahmari.acer@outlook.com>
2022-01-03 15:16:12 +0330

    Update DESolver.jl

--- ProblemSet9/Codes/DiffEqs/DESolver.jl
+++ ProblemSet9/Codes/DiffEqs/DESolver.jl
@@ -2,15 +2,48 @@ module DiffEqSolver
 
 export EulerDES
 
-function EulerDES(; ḟ::Function, y₀::Real, x₀::Float64, x₁::Float64, h::Float64)
-    xcoll = hcat(x₀:h:x₁)
-    ycoll = Float64[y₀]
-    y = y₀
-    for x ∈ xcoll
-        y += h * ḟ(x, y)
-        push!(ycoll, y)
+function EulerDES(; ẋ::Function, x₀::Vector{Float64}, t₀::Float64, t₁::Float64, h::Float64)
+    tcoll = collect(t₀:h:t₁)
+    xcoll = Float64[x₀]
+    vcoll = Float64[ẋ(t, x₀)]
+    x = x₀
+    for t ∈ tcoll
+        x += h * ẋ(t, x)
+        push!(xcoll, x)
+        push!(vcoll, ẋ(t, x))
     end
-    return xcoll, ycoll
+    return tcoll, xcoll, vcoll
+end
+
+function EulerYaghoubDES(; ẋ::Function, x₀::Vector{Float64}, t₀::Float64, t₁::Float64, h::Float64)
+    x = x₀
+    v = v₀ = ẋ(t, x)
+    tcoll = collect(t₀:h:t₁)
+    xcoll = Float64[x₀]
+    vcoll = Float64[v₀]
+    for t ∈ tcoll
+        x += h * v
+        push!(xcoll, x)
+        v = ẋ(t, x)
+        push!(vcoll, v)
+    end
+    return tcoll, xcoll, vcoll
+end
+
+function EulerCromerDES(; ẍ::Function, x₀::Vector{Float64}, v₀::Vector{Float64}, t₀::Float64, t₁::Float64, h::Float64)
+    tcoll = collect(t₀:h:t₁)
+    xcoll = Float64[x₀]
+    vcoll = Float64[v₀]
+    x = x₀
+    v = v₀
+    for t ∈ tcoll
+        v += h * ẍ(t, x)
+        x += h * v
+        push!(xcoll, x)
+        push!(vcoll, v)
+    end
+
+    return ts, xs, vs
 end
 
 end
