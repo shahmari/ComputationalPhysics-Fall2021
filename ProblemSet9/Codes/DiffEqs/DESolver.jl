cea9cec7601e11d1f1b496e559f9ca6355527dd3
shahmari <shahmari.acer@outlook.com>
2022-01-06 18:53:18 +0330

    update cromer - eydes - add midpoint and rk2

--- ProblemSet9/Codes/DiffEqs/DESolver.jl
+++ ProblemSet9/Codes/DiffEqs/DESolver.jl
@@ -5,32 +5,28 @@ export EulerDES
 function EulerDES(; ẋ::Function, x₀::Vector{Float64}, t₀::Float64, t₁::Float64, h::Float64)
     tcoll = collect(t₀:h:t₁)
     xcoll = Float64[x₀]
-    vcoll = Float64[ẋ(t, x₀)]
     x = x₀
     for t ∈ tcoll
         x += h * ẋ(t, x)
         push!(xcoll, x)
-        push!(vcoll, ẋ(t, x))
     end
-    return tcoll, xcoll, vcoll
+    return tcoll, xcoll
 end
 
 function EulerYaghoubDES(; ẋ::Function, x₀::Vector{Float64}, t₀::Float64, t₁::Float64, h::Float64)
-    x = x₀
-    v = v₀ = ẋ(t, x)
     tcoll = collect(t₀:h:t₁)
     xcoll = Float64[x₀]
-    vcoll = Float64[v₀]
+    x = x₀
+    ẋval = ẋ(t, x₀)
     for t ∈ tcoll
-        x += h * v
+        x += h * ẋval
+        ẋval = ẋ(t, x)
         push!(xcoll, x)
-        v = ẋ(t, x)
-        push!(vcoll, v)
     end
-    return tcoll, xcoll, vcoll
+    return tcoll, xcoll
 end
 
-function EulerCromerDES(; ẍ::Function, x₀::Vector{Float64}, v₀::Vector{Float64}, t₀::Float64, t₁::Float64, h::Float64)
+function EulerCromerDES(; ẍ::Function, x₀::Vector{Float64}, t₀::Float64, t₁::Float64, h::Float64)
     tcoll = collect(t₀:h:t₁)
     xcoll = Float64[x₀]
     vcoll = Float64[v₀]
@@ -43,7 +39,39 @@ function EulerCromerDES(; ẍ::Function, x₀::Vector{Float64}, v₀::Vector{Flo
         push!(vcoll, v)
     end
 
-    return ts, xs, vs
+    return tcoll, xcoll, vcoll
+end
+
+function RK2DES(; ẋ::Function, x₀::Vector{Float64}, t₀::Float64, t₁::Float64, h::Float64)
+    tcoll = collect(t₀:h:t₁)
+    xcoll = Float64[x₀]
+    x = x₀
+    for t ∈ tcoll
+        K₁ = ẋ(t, x) * h
+        K₂ = h * ẋ(t + h / 2, x + (K₁ * h) / 2)
+        x += K₂
+        push!(xcoll, x)
+    end
+    return tcoll, xcoll
+end
+
+
+function EulerMidPiontDES(; ẍ::Function, x₀::Vector{Float64}, v₀::Vector{Float64}, t₀::Float64, t₁::Float64, h::Float64)
+    tcoll = collect(t₀:h:t₁)
+    xcoll = Float64[x₀]
+    vcoll = Float64[v₀-(ẍ(t₀, initx)*h/2)]
+    x = x₀
+    v = v₀
+
+    for t ∈ tcoll
+        v += step * ẍ(t, x)
+        x += step * v
+
+        push!(xcoll, x)
+        push!(vcoll, v)
+    end
+
+    return tcoll, xcoll, vcoll
 end
 
 end
