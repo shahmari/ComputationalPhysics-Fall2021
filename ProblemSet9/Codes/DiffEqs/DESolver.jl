f2735cc0731e42628310fba933d6240e70ed9e8f
shahmari <shahmari.acer@outlook.com>
2022-01-06 20:14:49 +0330

    Update DESolver.jl

--- ProblemSet9/Codes/DiffEqs/DESolver.jl
+++ ProblemSet9/Codes/DiffEqs/DESolver.jl
@@ -1,12 +1,12 @@
-module DiffEqSolver
+module DiffEqsSolver
 
-export EulerDES
+export EulerDES, EulerYaghoubDES, EulerCromerDES, RK2DES, EulerMidPiontDES
 
 function EulerDES(; ẋ::Function, x₀::Vector{Float64}, t₀::Float64, t₁::Float64, h::Float64)
     tcoll = collect(t₀:h:t₁)
-    xcoll = Float64[x₀]
+    xcoll = [x₀]
     x = x₀
-    for t ∈ tcoll
+    for t ∈ tcoll[2:end]
         x += h * ẋ(t, x)
         push!(xcoll, x)
     end
@@ -15,10 +15,10 @@ end
 
 function EulerYaghoubDES(; ẋ::Function, x₀::Vector{Float64}, t₀::Float64, t₁::Float64, h::Float64)
     tcoll = collect(t₀:h:t₁)
-    xcoll = Float64[x₀]
+    xcoll = [x₀]
     x = x₀
     ẋval = ẋ(t, x₀)
-    for t ∈ tcoll
+    for t ∈ tcoll[2:end]
         x += h * ẋval
         ẋval = ẋ(t, x)
         push!(xcoll, x)
@@ -26,13 +26,26 @@ function EulerYaghoubDES(; ẋ::Function, x₀::Vector{Float64}, t₀::Float64, 
     return tcoll, xcoll
 end
 
-function EulerCromerDES(; ẍ::Function, x₀::Vector{Float64}, t₀::Float64, t₁::Float64, h::Float64)
+function RK2DES(; ẋ::Function, x₀::Vector{Float64}, t₀::Float64, t₁::Float64, h::Float64)
+    tcoll = collect(t₀:h:t₁)
+    xcoll = [x₀]
+    x = x₀
+    for t ∈ tcoll[2:end]
+        K₁ = ẋ(t, x) * h
+        K₂ = h * ẋ(t + h / 2, x + (K₁ * h) / 2)
+        x += K₂
+        push!(xcoll, x)
+    end
+    return tcoll, xcoll
+end
+
+function EulerCromerDES(; ẍ::Function, x₀::Float64, t₀::Float64, t₁::Float64, h::Float64)
     tcoll = collect(t₀:h:t₁)
     xcoll = Float64[x₀]
     vcoll = Float64[v₀]
     x = x₀
     v = v₀
-    for t ∈ tcoll
+    for t ∈ tcoll[2:end]
         v += h * ẍ(t, x)
         x += h * v
         push!(xcoll, x)
@@ -42,31 +55,17 @@ function EulerCromerDES(; ẍ::Function, x₀::Vector{Float64}, t₀::Float64, t
     return tcoll, xcoll, vcoll
 end
 
-function RK2DES(; ẋ::Function, x₀::Vector{Float64}, t₀::Float64, t₁::Float64, h::Float64)
-    tcoll = collect(t₀:h:t₁)
-    xcoll = Float64[x₀]
-    x = x₀
-    for t ∈ tcoll
-        K₁ = ẋ(t, x) * h
-        K₂ = h * ẋ(t + h / 2, x + (K₁ * h) / 2)
-        x += K₂
-        push!(xcoll, x)
-    end
-    return tcoll, xcoll
-end
-
-
-function EulerMidPiontDES(; ẍ::Function, x₀::Vector{Float64}, v₀::Vector{Float64}, t₀::Float64, t₁::Float64, h::Float64)
+function EulerMidPiontDES(; ẍ::Function, x₀::Float64, v₀::Float64, t₀::Float64, t₁::Float64, h::Float64)
     tcoll = collect(t₀:h:t₁)
     xcoll = Float64[x₀]
     vcoll = Float64[v₀-(ẍ(t₀, initx)*h/2)]
     x = x₀
     v = v₀
 
-    for t ∈ tcoll
+    for t ∈ tcoll[2:end]
         v += step * ẍ(t, x)
         x += step * v
-
+    
         push!(xcoll, x)
         push!(vcoll, v)
     end
