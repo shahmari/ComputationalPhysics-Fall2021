dec8295a7649fb28e1d9c4fa7d37b886eb0d0262
shahmari <shahmari.acer@outlook.com>
2022-01-17 14:42:34 +0330

    Update Q1-MD.jl

--- ProblemSet10/Codes/Q1/Q1-MD.jl
+++ ProblemSet10/Codes/Q1/Q1-MD.jl
@@ -2,6 +2,7 @@
 using Statistics: mean
 
 mutable struct MDSystem{DT<:AbstractFloat}
+    h::DT
     l::DT
     U::DT
     K::DT
@@ -11,13 +12,13 @@ mutable struct MDSystem{DT<:AbstractFloat}
     f::Matrix{DT}
     r::Matrix{DT}
     v::Matrix{DT}
-    function MDSystem(N::Integer, T₀::DT, l::DT) where {DT<:AbstractFloat}
+    function MDSystem(N::Integer, T₀::DT, l::DT, h) where {DT<:AbstractFloat}
         r = rand(DT, 2, N) * l
         v = rand(DT, 2, N) .- 0.5
         v .-= mean(v, dims = 2)
         v *= √(T₀ ÷ mean(v .^ 2))
         f = Matrix{DT}(undef, 2, N)
-        return new{DT}(DT(NaN), DT(NaN), DT(NaN), DT(NaN), DT(NaN), N, f, r, v)
+        return new{DT}(h, DT(NaN), DT(NaN), DT(NaN), DT(NaN), DT(NaN), N, f, r, v)
     end
 end
 
@@ -32,10 +33,23 @@ function update_force!(sys::MDSystem)
     end
 end
 
+function get_force(sys::MDSystem, i)
+    fᵢ = sys.DT[0.0, 0.0]
+    for j ∈ setdiff(1:sys.N, i)
+        Δr = √sum(abs.(sys.r[:, i] - sys.r[:, j]) .^ 2)
+        fᵢ += 48 * (((Δr)^-14) - 0.5 * ((Δr)^-8)) * abs.(sys.r[:, i] - sys.r[:, j])
+    end
+    return fᵢ
+end
+
 function update_phase!(sys::MDSystem)
     for i ∈ 1:sys.N
-        for j ∈ setdiff(1:sys.N, i)
-
+        f₁ = sys.f[:, i]
+        sys.r[:, i] += sys.h * sys.v[:, i] + 0.5 * sys.h * sys.h * f₁
+        f₂ = get_force(sys, i)
+        sys.f[:, i] = f₂
+        sys.v[:, i] += sys.h * (f₁ + f₂) / 2
+    end
 end
 
 function update_potential!(sys::MDSystem)
