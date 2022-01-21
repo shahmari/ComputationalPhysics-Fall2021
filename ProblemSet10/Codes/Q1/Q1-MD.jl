96c218c0c29cbc716e79e075f82c856c21c4472f
shahmari <shahmari.acer@outlook.com>
2022-01-21 16:46:50 +0330

    Update Q1-MD.jl

--- ProblemSet10/Codes/Q1/Q1-MD.jl
+++ ProblemSet10/Codes/Q1/Q1-MD.jl
@@ -3,8 +3,8 @@ using Statistics: mean
 using Plots
 
 mutable struct MDSystem{DT<:AbstractFloat}
-    h::DT
     l::DT
+    h::DT
     U::DT
     K::DT
     T::DT
@@ -13,20 +13,21 @@ mutable struct MDSystem{DT<:AbstractFloat}
     f::Matrix{DT}
     r::Matrix{DT}
     v::Matrix{DT}
-    function MDSystem(N::Integer, T₀::DT, l::DT, h::DT) where {DT<:AbstractFloat}
+    function MDSystem(N::Integer, T₀::DT, h::DT, l::DT) where {DT<:AbstractFloat}
         r = rand(DT, 2, N) * l
         v = rand(DT, 2, N) .- 0.5
         v .-= mean(v, dims = 2)
         v *= √(T₀ ÷ mean(v .^ 2))
         f = Matrix{DT}(undef, 2, N)
-        return new{DT}(h, l, DT(NaN), DT(NaN), DT(NaN), DT(NaN), N, f, r, v)
+        return new{DT}(l, h, DT(NaN), DT(NaN), DT(NaN), DT(NaN), N, f, r, v)
     end
 end
 
-function init(; N::Integer, T₀::DT, l::DT, h::DT) where {DT<:AbstractFloat}
-    sys = MDSystem(N, T₀, l, h)
+function init(; N::Integer, T₀::DT, h::DT, l::DT) where {DT<:AbstractFloat}
+    sys = MDSystem(N, T₀, h, l)
     update_forces!(sys)
     update_phase!(sys)
+    check_boundary!(sys)
     update_potential!(sys)
     update_kinetic!(sys)
     update_temperature!(sys)
@@ -36,6 +37,7 @@ end
 
 function simulate!(sys::MDSystem)
     update_phase!(sys)
+    check_boundary!(sys)
     update_potential!(sys)
     update_kinetic!(sys)
     update_temperature!(sys)
@@ -44,10 +46,21 @@ end
 
 function update_forces!(sys::MDSystem)
     for i ∈ 1:sys.N
-        sys.f *= 0
+        sys.f[:, i] *= 0
         for j ∈ setdiff(1:sys.N, i)
-            Δr = √sum(abs.(sys.r[:, i] - sys.r[:, j]) .^ 2)
-            sys.f[:, i] += 48 * (((Δr)^-14) - 0.5 * ((Δr)^-8)) * abs.(sys.r[:, i] - sys.r[:, j])
+            rmirror = sys.r[:, j]
+            if sys.r[1, i] - rmirror[1] > sys.l / 2
+                rmirror[1] -= sys.l
+            elseif sys.r[1, i] - rmirror[1] < -sys.l / 2
+                rmirror[1] += sys.l
+            end
+            if sys.r[2, i] - rmirror[2] > sys.l / 2
+                rmirror[2] -= sys.l
+            elseif sys.r[2, i] - rmirror[2] < -sys.l / 2
+                rmirror[2] += sys.l
+            end
+            Δr = √sum((sys.r[:, i] - rmirror) .^ 2)
+            sys.f[:, i] += 48 * (((Δr)^-14) - 0.5 * ((Δr)^-8)) * (sys.r[:, i] - rmirror)
         end
     end
 end
@@ -56,8 +69,19 @@ end
 function update_force!(sys::MDSystem, i::Integer)
     sys.f[:, i] *= 0
     for j ∈ setdiff(1:sys.N, i)
-        Δr = √sum(abs.(sys.r[:, i] - sys.r[:, j]) .^ 2)
-        sys.f[:, i] += 48 * (((Δr)^-14) - 0.5 * ((Δr)^-8)) * abs.(sys.r[:, i] - sys.r[:, j])
+        rmirror = sys.r[:, j]
+        if sys.r[1, i] - rmirror[1] > sys.l / 2
+            rmirror[1] -= sys.l
+        elseif sys.r[1, i] - rmirror[1] < -sys.l / 2
+            rmirror[1] += sys.l
+        end
+        if sys.r[2, i] - rmirror[2] > sys.l / 2
+            rmirror[2] -= sys.l
+        elseif sys.r[2, i] - rmirror[2] < -sys.l / 2
+            rmirror[2] += sys.l
+        end
+        Δr = √sum((sys.r[:, i] - rmirror) .^ 2)
+        sys.f[:, i] += 48 * (((Δr)^-14) - 0.5 * ((Δr)^-8)) * (sys.r[:, i] - rmirror)
     end
 end
 
@@ -78,17 +102,27 @@ function update_phase!(sys::MDSystem)
 end
 
 function check_boundary!(sys::MDSystem)
-    sys.r = (sys.r .+ sys.l) .% sys.l
+    sys.r = ((sys.r .% sys.l) .+ sys.l) .% sys.l
+end
 
 function update_potential!(sys::MDSystem)
     Σrterm = 0.0
 
     for i ∈ 1:(sys.N-1)
         for j ∈ i+1:sys.N
-            Δr = √sum(abs.(sys.r[:, i] - sys.r[:, j]) .^ 2)
-            if Δr < 2^(1 / 6)
-                Σrterm += ((Δr)^-12) - ((Δr)^-6)
+            rmirror = sys.r[:, j]
+            if sys.r[1, i] - rmirror[1] > 0.5
+                rmirror[1] -= 1
+            elseif sys.r[1, i] - rmirror[1] < -0.5
+                rmirror[1] += 1
             end
+            if sys.r[2, i] - rmirror[2] > 0.5
+                rmirror[2] -= 1
+            elseif sys.r[2, i] - rmirror[2] < -0.5
+                rmirror[2] += 1
+            end
+            Δr = √sum((sys.r[:, i] - rmirror) .^ 2)
+            Σrterm += ((Δr)^-12) - ((Δr)^-6)
         end
     end
 
@@ -112,7 +146,7 @@ function update_pressure!(sys::MDSystem)
     for i ∈ 1:(sys.N-1)
         for j ∈ i+1:sys.N
             Δr = √sum(abs.(sys.r[:, i] - sys.r[:, j]) .^ 2)
-            if Δr < 2^(1 / 6)
+            if Δr < 0.5
                 Σrterm += ((Δr)^-12) - 0.5 * ((Δr)^-6)
             end
         end
@@ -122,16 +156,17 @@ end
 
 
 
-Parameters = Dict(:N => 30, :T₀ => 200.0, :l => 1.0, :h => 0.01)
+Parameters = Dict(:N => 30, :T₀ => 100.0, :h => 0.01, :l => 30.0)
 sys = init(; Parameters...)
-[Tuple(sys.r[:, n]) for n ∈ 1:sys.N]
-scatter([Tuple(sys.r[:, n]) for n ∈ 1:sys.N])
-
-
-
-
-sys.r
-
 
 @gif for i ∈ 1:50
-    sca
+    simulate!(sys)
+    scatter([Tuple(sys.r[:, n]) for n ∈ 1:sys.N], xlims = (-1, 30.0), ylims = (-1, 30.0))
+end
+
+
+sys.v
+
+simulate!(sys)
+
+
